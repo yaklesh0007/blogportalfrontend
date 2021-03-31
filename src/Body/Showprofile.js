694ac21e931b6debc92fec559629d56d13efe3aa
aklesh <yaklesh0007@gmail.com>
2021-03-31 22:51:35 +0545

    added some icons from material ui icon

--- src/Body/Showprofile.js
+++ src/Body/Showprofile.js
@@ -4,6 +4,11 @@ import {Col, Row, CardTitle, CardSubtitle, CardText,CardBody,Card,Button,Input,
 Label,FormGroup,Form,CardImg
 } from 'reactstrap'
 import {Redirect} from 'react-router-dom'
+import EmailIcon from '@material-ui/icons/Email';
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
+import ExposureIcon from '@material-ui/icons/Exposure';
+import PhoneIcon from '@material-ui/icons/Phone';
+import PhotoIcon from '@material-ui/icons/Photo';
 export default class Showprofile extends Component {
     state={
         email:"",
@@ -47,21 +52,21 @@ export default class Showprofile extends Component {
             <div>
                 <Row>
                     <Col>
-                    <h3 className="text-success">Your profile</h3>
+                    <h3 className="text-success card-header">Update Your Profile</h3>
                  <Form>
-      <FormGroup>
-        <Label for="email">Email</Label>
+      {/* <FormGroup>
+        <Label for="email" className="text-primary"><EmailIcon color="secondary" className="mr-2"></EmailIcon><b>Email</b></Label>
         <Input type="email" name="email" id="email" placeholder="Enter Email"
         value={this.state.email} onChange={this.inputhandler}/>
-      </FormGroup>
+      </FormGroup> */}
       <FormGroup>
-        <Label for="username">Username</Label>
+        <Label for="username" className="text-primary"><AccountBoxIcon color="secondary" className="mr-2"></AccountBoxIcon><b>Username</b></Label>
         <Input type="text" name="username" id="username" placeholder="Enter Username"
         value={this.state.username} onChange={this.inputhandler}/>
       </FormGroup>
       
       <FormGroup>
-        <Label for="exampleSelect">Gender</Label>
+        <Label for="exampleSelect" className="text-primary"><ExposureIcon color="secondary" className="mr-2"></ExposureIcon><b>Gender</b></Label>
         <Input type="select" name="gender" placeholder="Select Your " value={this.state.gender}
                     onChange={this.inputhandler} required>
                         <option value="">Choose</option>
@@ -73,31 +78,31 @@ export default class Showprofile extends Component {
                        </Input>
       </FormGroup>
       <FormGroup>
-        <Label for="exampleFile">File</Label>
+        <Label for="exampleFile" className="text-primary "><PhotoIcon color="secondary" className="mr-2"></PhotoIcon><b>File</b></Label>
         <Input type="file" name="image" id="exampleFile"
             onChange={this.filehandler}
         />
         
       </FormGroup>
       <FormGroup>
-        <Label for="phone">Phone</Label>
+        <Label for="phone" className="text-primary"><PhoneIcon color="secondary" className="mr-2"></PhoneIcon><b>Phone</b></Label>
         <Input type="number" name="phone" id="Phone" value={this.state.phone}
             onChange={this.inputhandler}
         />
       </FormGroup>
       
       
-      <Button onClick={this.updateprofile} color="primary" block>Submit</Button>
+      <Button onClick={this.updateprofile} color="primary" block>Update profile</Button>
     </Form>
                     </Col>
                     <Col>
                     <Card>
                     <CardImg top className="image_profile" src={this.state.image} alt={this.state.username} />
                     <CardBody>
-                    <CardTitle tag="h6" >Email: {this.state.email}</CardTitle>
-                    <CardSubtitle tag="h6" >Username: {this.state.username}</CardSubtitle>
-                    <CardText>Gender: {this.state.gender}</CardText>
-                    <CardText>Phone: {this.state.phone}</CardText>
+                    <CardTitle tag="h6" ><EmailIcon color="secondary" className="mr-2"></EmailIcon> {this.state.email}</CardTitle>
+                    <CardSubtitle tag="h6" ><AccountBoxIcon color="secondary" className="mr-2"></AccountBoxIcon> {this.state.username}</CardSubtitle>
+                    <CardText><ExposureIcon color="secondary" className="mr-2"></ExposureIcon>{this.state.gender}</CardText>
+                    <CardText><PhoneIcon color="secondary" className="mr-2"></PhoneIcon>{this.state.phone}</CardText>
                     
                     </CardBody>
                      </Card>
