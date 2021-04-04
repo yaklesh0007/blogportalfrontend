24f5f859fa4aa2c4a7e98e09e0257c69c7795d9c
aklesh <yaklesh0007@gmail.com>
2021-04-04 20:10:31 +0545

    added reply and show reply

--- src/Body/Comment.js
+++ src/Body/Comment.js
@@ -1,5 +1,8 @@
 import React, { Component,state,inputhandler,addcomment,showcomments, deltecomment,getsinglecomment
-,open,handleEventChange,updateComment } from 'react'
+,open,handleEventChange,updateComment,showreplies,
+handleReplyChange,
+addreply,
+addreplys,cancelmodal} from 'react'
 import {Card,CardBody,CardFooter,CardHeader,
     CardTitle,Col,UncontrolledDropdown,DropdownToggle,
     DropdownMenu,DropdownItem,CardText, Button,
@@ -28,11 +31,15 @@ export default class Comment extends Component {
         config : {
             headers : {'authorization': `Bearer ${localStorage.getItem('token')}`}
         },
+        
         comments:[],
         replys:[],
         showcomment:false,
         isedit:false,
-        currentcomment:{}
+        currentcomment:{},
+        showreply:false,
+        opensecondmodal:false,
+        currentreply:{}
     }
 
     inputhandler=(e)=>{
@@ -43,6 +50,11 @@ export default class Comment extends Component {
     handleEventChange = (e) => {
         this.setState({
             currentcomment: { ...this.state.currentcomment, [e.target.name]: e.target.value }
+        })
+    }
+    handleReplyChange = (e) => {
+        this.setState({
+            currentreply: { ...this.state.currentcomment, [e.target.name]: e.target.value }
         })
     }
     componentDidMount(){
@@ -68,6 +80,11 @@ export default class Comment extends Component {
     open=()=>{
         this.setState({
             isedit:!this.state.isedit
+        })
+    }
+    cancelmodal=()=>{
+        this.setState({
+            opensecondmodal:!this.state.opensecondmodal
         })
     }
     addcomment= (e) =>{
@@ -135,6 +152,44 @@ export default class Comment extends Component {
             .catch((err)=>{
                 alert("not allowed to edit the post !!")
             })
+     }
+     showreplies=(commentID)=>{
+         
+         axios.get('http://localhost:90/reply/'+commentID, this.state.config)
+         .then((responce)=>{
+            
+            this.setState({
+                showreply:!this.state.showreply,
+                replys:responce.data.data
+            })
+            console.log(this.state.showreply)
+         })
+         .catch((error)=>{
+                alert("donot have replies !!")
+         })
+     }
+     addreply=(commentID)=>{
+        this.setState({
+            opensecondmodal:!this.state.opensecondmodal,
+            commentID:commentID
+        })
+        console.log(commentID)
+     }
+     addreplys=(e)=>{
+         e.preventDefault()
+         const data={
+             commentID:this.state.commentID,
+             replyBody:this.state.replyBody
+         }
+         console.log(data)
+         axios.post('http://localhost:90/reply/insert', data, this.state.config)
+         .then((response)=>{
+            alert("Successfully added reply !!!")
+            window.location.href='/'
+         })
+         .catch((err)=>{
+             alert("unable to add reply")
+         })
      }
     render() {
         return (
@@ -239,12 +294,22 @@ export default class Comment extends Component {
                                         <DropdownItem><button 
                                         onClick={this.deltecomment.bind(this, commente._id,commente.userID._id)}
                                         className="btn btn-danger"><DeleteIcon></DeleteIcon>Delete</button></DropdownItem>
+                                        <DropdownItem><button 
+                                        onClick={this.addreply.bind(this, commente._id)}
+                                        className="btn btn-primary"><DeleteIcon></DeleteIcon>Add reply</button></DropdownItem>
+                                        <DropdownItem><button 
+                                        onClick={this.showreplies.bind(this, commente._id)}
+                                        className="btn btn-primary"><DeleteIcon></DeleteIcon>Show</button></DropdownItem>
+
                                 </DropdownMenu>
+                                        
                                 </UncontrolledDropdown>
                             </Col>
                             </Row>
                             </CardHeader>
+                            
                             </div>
+                            
                             )
          })
       }
@@ -252,6 +317,39 @@ export default class Comment extends Component {
                    
                     :null
                     }
+                    {this.state.showreply?
+                            <div>
+                            <p>Replies</p>
+                            {this.state.replys.map((rply)=>{
+                            return(
+                            <CardHeader>
+                                <Row >
+                                <Col xs="1" >
+                                    <img className="img-round" alt={rply.userID.username} 
+                                    src=
+                                    {'http://localhost:90/images/'+rply.userID.image}
+                                    ></img>
+                                </Col>
+                                <Col xs="4">
+                                    <p className="text-success mt-3 float-left"><i><u>#
+                                    {rply.userID.username}
+                                    </u></i></p>
+                                </Col>
+                                <Col xs="4">
+                                    <p><b>{rply.replybody}</b></p>
+                                </Col>
+                                <Col xs="3">
+                                </Col>
+                                </Row>
+                                </CardHeader>
+				                )  
+                                })
+                                }
+                            </div>
+                              :null
+                    }
+
+
                     <Modal isOpen={this.state.isedit}
                     
                      >
@@ -268,12 +366,35 @@ export default class Comment extends Component {
                            </Button>
                        </Form>
                         </ModalBody>
-
                         <ModalFooter>
-                        
                         <Button className="btn btn-danger" onClick={this.open}>Cancel</Button>
                         </ModalFooter>
                     </Modal>
+
+                    <Modal isOpen={this.state.opensecondmodal}
+                    
+                    >
+                       <ModalHeader >Add Reply</ModalHeader>
+                       <ModalBody>
+                      <Form>
+                          <Input name="replyBody" value={this.state.replyBody} type="text"
+                          onChange={this.inputhandler}></Input>
+                          <Input type="hidden" value={this.state.commentID} name="commentID" />
+                          <Button className="btn btn-primary mt-2" onClick={this.addreplys} >
+                               Save changes
+                          </Button>
+                      </Form>
+                       </ModalBody>
+                       <ModalFooter>
+                       <Button className="btn btn-danger" onClick={this.cancelmodal}>Cancel</Button>
+                       </ModalFooter>
+                   </Modal>
+                   <Modal open={this.state.showreply}>
+                       <ModalHeader>All the Comment on that Comment</ModalHeader>
+                       <ModalBody>
+                       
+                       </ModalBody>
+                   </Modal>
             </div>
         )
     }
